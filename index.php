d37a85461cf007d7ac19b962e120a5067f63ee8c
Paulo <ppauloces27@gmail.com>
2022-04-03 15:38:58 -0300

    init

---
+++ index.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>e-Ofício - Gerador de ofícios on-line!</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="assets/lading-page/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="assets/lading-page/css/lading.css">
+  <link href="assets/lading-page/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  
+</head>
+<body>
+
+
+  <div class="navbar-fixed">
+    <nav class="gradiente-fundo" role="navigation">
+      <div class="nav-wrapper">
+        <img src="assets/logo.png" width="60px">
+        <ul class="right hide-on-med-and-down">
+          <li><a href="sass.html" style="color:white">Tecnologias</a></li>
+          <li><a href="badges.html" style="color:white">Planos de estudo</a></li>
+          <li><a href="collapsible.html" style="color:white">Depoimentos</a></li>
+          <li><a href="collapsible.html" style="color:white">Plano de Estudo</a></li>
+          <li><a href="collapsible.html" style="color:white">Contato</a></li>
+          <li>
+            <a href="login.php" class="waves-effect waves-light btn nav-btn-cadastro">ME MATRICULAR</a>
+          </li>
+        </ul>
+
+        <ul id="nav-mobile" class="sidenav gradiente-fundo">
+          <br>
+          <li><a href="sass.html" style="color:white">Tecnologias</a></li>
+          <hr class="container">
+          <li><a href="badges.html" style="color:white">Planos de estudo</a></li>
+          <hr class="container">
+          <li><a href="collapsible.html" style="color:white">Depoimentos</a></li>
+          <hr class="container">
+          <li><a href="collapsible.html" style="color:white">Plano de Estudo</a></li>
+          <hr class="container">
+          <li><a href="collapsible.html" style="color:white">Contato</a></li>
+          <hr class="container">
+         <li>
+            <a href="login.php" class="waves-effect waves-light btn nav-btn-cadastro">ME MATRICULAR</a>
+          </li>
+        </ul>
+        <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons" style="color: #212529">menu</i></a>
+      </div>
+    </nav>
+  </div>
+
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <div class="row center">
+          <img src="assets/logo.png" width="120px" height="100px" align="">
+          <h3 class="header col s12 cafe-text" style="font-weight: 900">APRENDA CRUD DE UMA FORMA FÁCIL DE UMA VEZ POR TODAS!</h3>
+        </div>
+        <div class="row center">
+          <div class="col s6 offset-m3">
+           <div class="video-container">
+            <iframe src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe>
+          </div>
+        </div>
+        <br>
+      </div>
+      <br><br>
+
+    </div>
+  </div>
+  <div class="parallax"><img src="assets/lading-page/bg_1.jpg" alt="pessoa escrevendo" style="filter: blur(4px);"></div>
+</div>
+
+
+<div class="container">
+  <div class="section">
+
+    <!--   Icon Section   -->
+    <div class="row">
+      <div class="col s12 m4">
+        <div class="icon-block">
+          <h2 class="center" id="gradient_icon"><i class="material-icons">flash_on</i></h2>
+          <h5 class="center" style="font-weight:700">Agilidade</h5>
+
+          <p class="light center" style="font-weight:500">No nosso sistema, a geração de ofícios é um processo rápido e ágil, economizando bastante o seu tempo.</p>
+        </div>
+      </div>
+
+      <div class="col s12 m4">
+        <div class="icon-block">
+          <h2 class="center" id="gradient_icon"><i class="material-icons">group</i></h2>
+          <h5 class="center" style="font-weight:700">Experiência de usuário</h5>
+
+          <p class="light center" style="font-weight:500">Nele você desfrutará de todos os recursos prometendo a maior experiência na geração de ofícios.</p>
+        </div>
+      </div>
+
+      <div class="col s12 m4">
+        <div class="icon-block">
+          <h2 class="center" id="gradient_icon"><i class="material-icons">settings</i></h2>
+          <h5 class="center" style="font-weight:700">Fácil de trabalhar</h5>
+
+          <p class="light center" style="font-weight:500">O sistema e-Ofício possui ferramentas simples de fácil compreendimento, e mesmo assim você terá um suporte geral com nossa equipe.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="parallax-container valign-wrapper">
+  <div class="section no-pad-bot">
+    <div class="container">
+      <div class="row center">
+        <!--<h5 class="header col s12" style="color:#212121">Cadastre-se, automatize seus ofícios enquanto você toma um bom café!</h5>-->
+      </div>
+    </div>
+  </div>
+  <div class="parallax"><img src="assets/lading-page/bg_2.jpg" alt="pessoa escrevendo" style="filter: blur(4px);"></div>
+</div>
+
+<div class="container">
+  <div class="section">
+    <div class="row">
+      <div class="col s12 m3">
+        <div class="card blue-grey white">
+          <h2 class="center" id="gradient_icon"><i class="material-icons size-icons">receipt_long</i></h2>
+          <h5 class="center count" style="font-weight:600">15789</h5>
+          <div class="card-content white-text">
+            <p class="center black-text">Ofícios emitidos</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m3">
+        <div class="card blue-grey white">
+          <h2 class="center" id="gradient_icon"><i class="material-icons size-icons">domain</i></h2>
+          <h5 class="center count" style="font-weight:600">502</h5>
+          <div class="card-content white-text">
+            <p class="center black-text">Empresas cadastradas</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m3">
+        <div class="card blue-grey white">
+          <h2 class="center" id="gradient_icon"><i class="material-icons size-icons">group</i></h2>
+          <h5 class="center count" style="font-weight:600">7890</h5>
+          <div class="card-content white-text">
+            <p class="center black-text">Clientes felizes</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col s12 m3">
+        <div class="card blue-grey white">
+          <h2 class="center" id="gradient_icon"><i class="material-icons size-icons">public</i></h2>
+          <h5 class="center count" style="font-weight:600">13</h5>
+          <div class="card-content white-text">
+            <p class="center black-text">Países conosco</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div> 
+
+
+
+
+<div class="container">
+  <div class="section">
+
+    <div class="row">
+      <div class="col s12 center">
+        <h3><i class="mdi-content-send brown-text"></i></h3>
+        <h4 style="font-weight:700">Planos</h4>
+        <br>
+        <div class="col s12 m4">
+
+          <div class="card pricing style-2">
+            <div class="plan-title waves-effect waves-block waves-light gradiente-fundo">
+              <h5 class="white-text">
+                <span class="plan" style="font-size: 20px;">DEMONSTRAÇÃO</span>
+              </h5>
+            </div>
+
+            <div class="price waves-effect waves-block waves-light gradiente-fundo">
+              <h4 class="white-text">
+                <span class="currency">15 DIAS GRÁTIS</span>
+              </h4>
+            </div>
+
+            <div class="card-content">
+              <ul class="collection">
+                <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper"><i class="material-icons left" id="gradient_icon">done</i>Usuários ilimitados</p></li>
+                <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper"><i class="material-icons left" id="gradient_icon">done</i>Oficios ilimitados</p></li>
+                <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper"><i class="material-icons left" id="gradient_icon">done</i>Suporte por Email</p></li>
+                <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper" ><i class="material-icons left" id="gradient_icon">done</i>Suporte por Whatsapp</p></li>
+                <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper" ><i class="material-icons left" id="gradient_icon">done</i>Envio por e-mail</p></li>             
+              </ul>
+              <div class="plan-signup-btn">
+                <a id="start-now-premium" class="btn waves-effect waves-block waves-light no-margin white-text nav-btn-cadastro" href="cadastro.php?" >Adicionar ao carrinho</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12 m4">
+          <div class="card pricing style-2 z-depth-4">
+            <div class="plan-title waves-effect waves-block waves-light gradiente-ouro">
+              <h5 class="dark-grey-text">
+                <span class="plan" style="font-size: 25px;">PROFISSIONAL</span>
+              </h5>
+            </div>
+            <div class="price waves-effect waves-block waves-light gradiente-ouro">
+              <h4 class="dark-grey-text">
+                <span class="currency">R$</span>
+                <span class="amount">23.99</span>
+                <span class="period">/mês</span>
+              </h4>
+            </div>
+            <div class="card-content">
+              <ul class="collection">
+                <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper"><i class="material-icons left" id="gradient_icon">done</i>Até 50 Usuários</p></li>
+                <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper"><i class="material-icons left" id="gradient_icon">done</i>Ofícios ilimitados</p></li>
+                <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper"><i class="material-icons left" id="gradient_icon">done</i>Suporte por Email</p></li>
+                <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper" ><i class="material-icons left" id="gradient_icon">done</i>Suporte por Whatsapp</p></li>
+                <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper"><i class="material-icons" id="gradient_icon">done</i>Envio por e-mail</p></li>               
+              </ul>
+            </ul>
+            <div class="plan-signup-btn">
+              <a id="start-now-plus" class="btn-large waves-effect waves-block waves-light no-margin white-text gradiente-ouro" href="#"><span class="dark-grey-text" style="font-size:14px">Adicionar ao carrinho</span></a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="col s12 m4">
+        <div class="card pricing style-2">
+
+          <div class="plan-title waves-effect waves-block waves-light gradiente-fundo">
+            <h5 class="white-text">
+              <span class="plan" style="font-size: 20px;">EMPRESARIAL</span>
+            </h5>
+          </div>
+
+          <div class="price waves-effect waves-block waves-light gradiente-fundo">
+            <h4 class="white-text">
+              <span class="currency">R$</span>
+              <span class="amount">23.99</span>
+              <span class="period">/mês</span>
+            </h4>
+          </div>
+
+          <div class="card-content">
+            <ul class="collection">
+              <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper"><i class="material-icons left" id="gradient_icon">done</i>Até 05 Usuários</p></li>
+              <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper"><i class="material-icons left" id="gradient_icon">done</i>Até 250 Ofícios/mês</p></li>
+              <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper"><i class="material-icons left" id="gradient_icon">done</i>Suporte por Email</p></li>
+              <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper" ><i class="material-icons left" id="gradient_icon">done</i>Suporte por Whatsapp</p></li>
+              <li data-tooltip="On demand increase anytime with extra fee" data-delay="10" data-position="top" class="collection-item tooltipped"><p class="valign-wrapper"><i class="material-icons " id="gradient_icon">done</i>Envio por e-mail</p></li>             
+            </ul>
+            <div class="plan-signup-btn">
+              <a id="start-now-premium" class="btn waves-effect waves-block waves-light no-margin white-text nav-btn-cadastro" href="">Adicionar ao carrinho</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+</div>
+
+
+<div class="parallax-container valign-wrapper">
+  <div class="section no-pad-bot">
+    <div class="container">
+      <div class="row center">
+        <!--<h5 class="header col s12" style="color:#000000  ">Cadastre-se, automatize seus ofícios enquanto você toma um bom café!</h5>-->
+      </div>
+    </div>
+  </div>
+  <div class="parallax"><img src="assets/lading-page/bg_3.jpg" alt="pessoa escrevendo" width="80px" style="filter: blur(4px);"></div>
+</div>
+
+<footer class="page-footer gradiente-fundo">
+  <div class="container">
+    <div class="row">
+      <div class="col l6 s12">
+        <h5 class="white-text footer-h5">e-Ofício</h5>
+        <p class="white-text" style="font-weight: 300;">O e-Ofício é um sistema automático gerador de ofícios. Com ele você pode gerar os documentos de forma prática e rápida, e do modelo que for necessário para sua situação</p>
+
+
+      </div>
+      <div class="col l3 s12">
+        <h5 class="white-text footer-h5">Contato</h5>
+        <ul>
+          <li class="material-icons valign-wrapper black-text footer-li">call<a class="black-text" href="#!"> (73)99999-9999</a></li>
+          <li class="material-icons valign-wrapper black-text footer-li">email<a class="black-text" href="#!"> drwatson@tecnologias.com</a></li>
+          <li class="material-icons black-text footer-li">map<a class="black-text" href="#!">Itagimirim, BA, Brasil</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="http://drwatson.com">Dr. Watson Tecnologias</a>
+    </div>
+  </div>
+</footer>
+
+
+<!--  Scripts-->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="assets/lading-page/js/materialize.js"></script>
+<script src="assets/lading-page/js/init.js"></script>
+
+<script type="text/javascript">
+  $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 6000,
+      easing: 'swing',
+      step: function (now) {
+        $(this).text(Math.ceil(now));
+      }
+    });
+  });
+</script>
+
+</body>
+</html>
